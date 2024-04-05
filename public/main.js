5b0b34ccd1b8644704bdd8454e871cfd965c08e9
Oluwaseyi <kdom22033@gmail.com>
2024-04-05 20:59:40 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -31,13 +31,13 @@ $('.team',).slick({
     autoplaySpeed: 5000,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
